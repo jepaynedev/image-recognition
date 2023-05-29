c2fcec1ecc1e1538a80df19fcbf74fa42bbeeb70
James Payne <github@jepaynedev.com>
2023-05-29 13:32:42 -0500

    Add basic project structure
    * pyproject.toml for metadata, dependencies and entry points
    * scripts module  for console scripts and environment setup tools

---
+++ src/scripts/dataset.py
@@ -0,0 +1,12 @@
+"""Console script to automate some dataset functionality
+
+Scripted aquisition and project setup for ease of use and reproducibility
+"""
+
+
+def main():
+    """Entry point for dataset console script"""
+
+
+if __name__ == '__main__':
+    main()
